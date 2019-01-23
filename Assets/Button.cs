3eeb1d6d50d5fd4e77113e72fa186e5456772b08
UAD\1502642 <lewisbowie@outlook.com>
2019-01-23 10:23:24 +0000

    added player movement with jump and fly ready to go

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -18,3 +18,4 @@ public class Button : MonoBehaviour {
     }
 
 }
+//hello
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     [Header("Player Movement")]
     public float speed = 10.0f;
-    public float gravity = 30.0f;
     public float FlySpeed = 12.0f;
+    public float JumpForce = 900.0f;
+    public int JumpHeight;
 
     //other variables
     private Vector2 moveDirection = Vector2.zero;
     private Vector2 fly;
+    private Vector2 jump;
     private Rigidbody2D controller;
+    private bool canJump;
+
+
 
     [Header("Checks")]
     public Transform GroundCheck;
-   
+
     void Start()
     {
         //gets the controller on start
@@ -27,32 +33,61 @@ public class PlayerController : MonoBehaviour {
         // Gravity of Game object
         gameObject.transform.position = new Vector3(-18, -8, 0);
 
-        //sets upforce to a Vector2
+        //Declares Fly as Vector2
         fly = new Vector2(0, 19.8f * FlySpeed);
+
+        //Declares jump force
+        jump = Vector2.up * JumpForce;
+        canJump = false;
     }
+
+    void Fly()
+    {
+        //flys character
+        GetComponent<Rigidbody2D>().AddForce(fly);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+
+    void Jump()
+    {
+        //Jumps character
+
+        canJump = true;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 900f));
+    }
+   
+        
 
     void Update()
     {
-        GetComponent<Rigidbody2D>().gravityScale = gravity;
+        
         //checks if the player is on grounds
         if (Physics2D.OverlapCircle(GroundCheck.position, 0.2f))
         {
             //moves character on x and y axis
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0.0f);
+            moveDirection = new Vector2(Input.GetAxis("Horizontal"), controller.velocity.y);
             //changes direction character faces to last key
             moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection = moveDirection * speed;
-
-            if (Input.GetKey(KeyCode.Space))
+            moveDirection.x *= speed;
+            //Sets jump on space and fly on double space
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-             //on spacebar moves character up, character falls if space let go
-                GetComponent<Rigidbody2D>().AddForce(fly);
-                GetComponent<Rigidbody2D>().gravityScale = 1;
-               
+                Jump();
             }
+
+
+
+            // moves the player
+            controller.velocity = (moveDirection);
         }
-     
-        // moves the player
-       controller.velocity = ( moveDirection);
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+       {
+            canJump = false;
+        }
+   }
 }
+
