9a977057884a3db7c9a64e8d158e39829fe43ac8
lewisbowie <lewisbowie@outlook.com>
2019-02-11 10:09:29 +0000

    added breath attack, added collectible drop when player killed and reworked movement slightly

--- Assets/Scripts/KnightAI.cs
+++ Assets/Scripts/KnightAI.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.SceneManagement;  public class KnightAI : MonoBehaviour {      [Header("Speed of character")]     public float speed;     public float distance;      [Header("Detections")]     public Transform GroundDetection;     public Transform WallDetection;     public Transform Player;      //private variables     private Vector2 direction = new Vector2(0, 1);     private bool moveRight = true;     private bool Move = true;     private Vector3 newXPosition;     private bool CheckGround = true;     private bool CheckWall = true;     private bool delay = false;     private float delayFloat;        [Header("Damage Options")]     public float AttackRangeMelee;     public int damage;     public float DelayAttack;     public float minDistance;     private float lastAttack;      [Header("Enemy Attributes")]     public float Health;      private void Start()
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.SceneManagement;  public class KnightAI : MonoBehaviour {      [Header("Speed of character")]     public float speed;     public float distance;      [Header("Detections")]     public Transform GroundDetection;     public Transform WallDetection;     public Transform Player;      //private variables     private Vector2 direction = new Vector2(0, 1);     private bool moveRight = true;     private bool Move = true;     private Vector3 newXPosition;     private bool CheckGround = true;     private bool CheckWall = true;     private bool delay = false;     private float delayFloat;     public GameObject Collectible;        [Header("Damage Options")]     public float AttackRangeMelee;     public int damage;     public float DelayAttack;     public float minDistance;     private float lastAttack;      [Header("Enemy Attributes")]     public float Health;      private void Start()
     {
-      
+        Move = true;         CheckWall = true;         CheckGround = true; 
     }
-
-
 
     // Update is called once per frame
     void Update()     {
@@ -11,7 +9,7 @@
 
         //moves enemy along platform
         if (Move)         {             NormalMove();         }
-        if (delay)         {             StartCoroutine("delayNormal");         }
+        if (delay)         {             StartCoroutine("delayNormal");             Move = true;             CheckWall = true;             CheckGround = true;         }
 
         //uses raycast to detect when platform edge is beside the enemy.
         if (CheckGround)         {             RaycastHit2D groundInfo = Physics2D.Raycast(GroundDetection.position, Vector2.down, distance);              if (groundInfo.collider == false)             {                 if (moveRight == true)                 {                     transform.eulerAngles = new Vector3(0, -180, 0);                     moveRight = false;                 }                 else                 {                     transform.eulerAngles = new Vector3(0, 0, 0);                     moveRight = true;                 }             }          }         if (CheckWall)         {             RaycastHit2D Wall = Physics2D.Raycast(WallDetection.position, direction, distance);              if (Wall == true)             {                 if (Wall.collider.CompareTag("Wall"))                 {
@@ -21,35 +19,20 @@
                     Rotate();                     speed *= -1;                     direction *= -1;                     moveRight = false;                 }             }         }
 
         //Attacking the Player melee
-        float distancefromPlayer = Vector2.Distance(transform.position, Player.position);           if (distancefromPlayer < AttackRangeMelee)         {             if (Time.time > lastAttack + DelayAttack)             {                 Player.SendMessage("TakeDamage", damage);                 lastAttack = Time.time;             }          }         if (distancefromPlayer < minDistance)         {
+        float distancefromPlayer = Vector2.Distance(transform.position, Player.position);           if (distancefromPlayer < AttackRangeMelee)         {             if (Time.time > lastAttack + DelayAttack)             {                 Player.SendMessage("TakeDamage", damage);                 lastAttack = Time.time;             }          }          if (distancefromPlayer < minDistance)         {
             //sets movement and wall and ground checks to false
             Move = false;             CheckWall = false;             CheckGround = false;
 
             //calls function to move enemy towards the player
             MovetoPlayer();
-            delay = false;             delayFloat = Time.deltaTime + 3;
-
-        }
-           else if (delayFloat < Time.deltaTime)         {             delay = true;         } 
-
-        else
-        //if(distancefromPlayer > minDistance)
-        {
-            //adds delay to the enemy going back to normal movement
-
-
-            //add animation cycles to attacks
-
-
-            //if the player is far enough away from the player then the enemy will continue their normal movement
-            Move = true;             CheckWall = true;             CheckGround = true;
-        }
-     
-
-
+            //delay = false;             //delayFloat = Time.deltaTime + 3; 
+        } 
+           else          {             delay = true;                     } 
     }      //changes rotation of enemy when colliding with wall     void Rotate()     {         Vector3 scale = transform.localScale;         scale.x *= -1;         transform.localScale = scale;     }      void MovetoPlayer()     {
         //moves the enemy to the player when in range
-                transform.position = Vector2.MoveTowards(transform.position, new Vector2(Player.position.x, transform.position.y), speed * Time.deltaTime);     }     void NormalMove()     {         //the normal movement for the enemy along the platform         transform.Translate(Vector2.right * speed * Time.deltaTime);     }     public void TakeDamage(int damage)     {         Health -= damage;         if (Health <= 0)         {             Destroy(gameObject);             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);         }     }      IEnumerator delayNormal(){        
-            yield return new WaitForSeconds(3);                                } } 
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(Player.position.x, transform.position.y), speed * Time.deltaTime);     }     void NormalMove()     {         //the normal movement for the enemy along the platform         transform.Translate(Vector2.right * speed * Time.deltaTime);     }     public void TakeDamage(int damage)     {         Health -= damage;         if (Health <= 0)         {             Destroy(gameObject);             Instantiate(Collectible, transform.position, transform.rotation);         }
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }       IEnumerator delayNormal(){        
+            yield return new WaitForSeconds(3);      }   } 
 
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine; using UnityEngine.SceneManagement;   public class PlayerController : MonoBehaviour {      [Header("Player Movement")]     public float speed = 10.0f;     public float FlySpeed = 12.0f;     public float JumpForce = 900.0f;     public float flyingTime = 2f;      //other variables     //private Vector2 moveDirection = Vector2.zero;     private Vector2 fly;     private Vector2 jump;     private Rigidbody2D controller;     private bool canJump;     private Vector3 flyingposition;     private bool canFly;     private float flyingTimer;     private int pickedUp;
     private float moveDirection;
     private bool facingRight;
-    private bool spriteFlip;      [Header("Enemy")]     public Transform Knight;      [Header("Gem Activations")]     public int FlyActivates;      [Header("Player Attributes")]     public float Health;      [Header("Melee Damage Options")]     public float AttackRangeMelee;     public int damage;     public float DelayAttack;     private float lastAttack;        [Header("Checks")]     public Transform GroundCheck;      void Start()     {         //gets the controller on start         controller = GetComponent<Rigidbody2D>();
+
+    public Animator PlayerTakesDamage;      [Header("Enemy")]     public Transform Knight;      [Header("Gem Activations")]     public int JumpActivates;
+    public int FlyActivates;
+    public int BreathAttackActivates;      [Header("Player Attributes")]     public float Health;      [Header("Melee Damage Options")]     public float AttackRangeMelee;     public int MeleeDamage;
+    public int BreathDamage;     public float DelayAttack;     private float lastAttackMelee;
+    public float AttackRangeBreath;
+    private float LastAttackBreath;        [Header("Checks")]     public Transform GroundCheck;      void Start()     {         //gets the controller on start         controller = GetComponent<Rigidbody2D>();
 
       
         facingRight = true;          // Gravity of Game object         gameObject.transform.position = new Vector3(-18, -8, 0);          //Declares Fly as Vector2         fly = new Vector2(0, 19.8f * FlySpeed);          //Declares jump force         jump = Vector2.up * JumpForce;         canJump = false;         canFly = false;     }      void Fly()     {         //flys character                  canFly = true;           flyingTimer = 0;         controller.gravityScale = 0.0f;     }      void Jump()     {         //Jumps character         GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 500f));     }
@@ -12,6 +18,7 @@ using UnityEngine; using UnityEngine.SceneManagement;   public class Pla
     {
         moveDirection = Input.GetAxis("Horizontal");
         controller.velocity = new Vector2(moveDirection * speed, controller.velocity.y);
+
     }
 
     void Update()     {         //checks if the player is on grounds         if (Physics2D.OverlapCircle(GroundCheck.position, 0.2f))         {                         //changes direction character faces to last key
@@ -20,11 +27,43 @@ using UnityEngine; using UnityEngine.SceneManagement;   public class Pla
                 facingRight = !facingRight;
                 Rotate();
             }
-       
-          
         }
 
-             //Sets jump on space             if (Input.GetKeyDown(KeyCode.Space))             {                 if (canJump)                 {                     Jump();                 }             }          //sets fly on hold m         if(pickedUp >= FlyActivates)         {             if (Input.GetKeyDown(KeyCode.M) && !canFly)             {                 Fly();             }              //returns if M is released flying stops             if (Input.GetKeyUp(KeyCode.M))             {                 canFly = false;                 controller.gravityScale = 5.0f;             }              if (canFly)             {                 //  flyingposition.x = moveDirection.x *speed * Time.deltaTime;                 flyingposition = transform.localPosition;                 flyingposition.y += 2;                  transform.position = Vector3.Lerp(transform.position, flyingposition, Time.deltaTime * 2);                 flyingTimer += Time.deltaTime;                  if (flyingTimer >= flyingTime)                 {                     canFly = false;                     controller.gravityScale = 5.0f;                 }             }         }                 //Melee Attack          if(Input.GetKeyDown(KeyCode.Z))         {             MeleeAttack();         }                    }            //Checks if player is grounded as to jump     void OnCollisionEnter2D(Collision2D collision)     {         if (collision.gameObject.tag == "Ground")        {             canJump = true;            // canFly = true;                    }    }     void OnCollisionExit2D(Collision2D collision)     {         if (collision.gameObject.tag == "Ground")         {             canJump = false;            // canFly = false;         }     }      //Adds collectible pick up     void OnTriggerEnter2D(Collider2D collider)     {         if(collider.gameObject.tag == "Collectible")         {             collider.gameObject.SetActive(false);             pickedUp += 1;         }     }       public void TakeDamage(int damage)     {         Health -= damage;         if(Health <= 0)         {             Destroy(gameObject);             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);         }      }     void MeleeAttack()     {         float distancefromPlayer = Vector2.Distance(transform.position, Knight.position);          if(GameObject.FindGameObjectWithTag("Enemy"))         {             if (distancefromPlayer < AttackRangeMelee)             {                  Knight.SendMessage("TakeDamage", damage);                 lastAttack = Time.time;              }         }             }
+        if (pickedUp >= JumpActivates)
+        {
+
+
+            //Sets jump on space
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (canJump)
+                {
+                    Jump();
+                }
+            }
+        }
+
+        //sets fly on hold m
+        if (pickedUp >= FlyActivates)         {             if (Input.GetKeyDown(KeyCode.M) && !canFly)             {                 Fly();             }              //returns if M is released flying stops             if (Input.GetKeyUp(KeyCode.M))             {                 canFly = false;                 controller.gravityScale = 5.0f;             }              if (canFly)             {                 //  flyingposition.x = moveDirection.x *speed * Time.deltaTime;                 flyingposition = transform.localPosition;                 flyingposition.y += 2;                  transform.position = Vector3.Lerp(transform.position, flyingposition, Time.deltaTime * 2);                 flyingTimer += Time.deltaTime;                  if (flyingTimer >= flyingTime)                 {                     canFly = false;                     controller.gravityScale = 5.0f;                 }             }         }                 //Melee Attack          if(Input.GetKeyDown(KeyCode.Z))         {             MeleeAttack();         }
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            BreathAttack();
+        }                 }            //Checks if player is grounded as to jump     void OnCollisionEnter2D(Collision2D collision)     {         if (collision.gameObject.tag == "Ground")        {             canJump = true;            // canFly = true;                    }    }     void OnCollisionExit2D(Collision2D collision)     {         if (collision.gameObject.tag == "Ground")         {             canJump = false;            // canFly = false;         }     }      //Adds collectible pick up     void OnTriggerEnter2D(Collider2D collider)     {         if(collider.gameObject.tag == "Collectible")         {             collider.gameObject.SetActive(false);             pickedUp += 1;         }     }       public void TakeDamage(int damage)     {         Health -= damage;
+        PlayerTakesDamage.Play("PlayerTakesDamage");         if(Health <= 0)         {             Destroy(gameObject);             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);         }      }     void MeleeAttack()     {         float distancefromPlayerMelee = Vector2.Distance(transform.position, Knight.position);          if(GameObject.FindGameObjectWithTag("Enemy"))         {             if (distancefromPlayerMelee < AttackRangeMelee)             {                  Knight.SendMessage("TakeDamage", MeleeDamage);                 lastAttackMelee = Time.time;             }         }             }
+
+    void BreathAttack()
+    {
+        float distancefromPlayerBreath = Vector2.Distance(transform.position, Knight.position);
+
+        if(GameObject.FindGameObjectWithTag("Enemy"))
+        {
+            if(distancefromPlayerBreath < AttackRangeBreath)
+            {
+                Knight.SendMessage("TakeDamage", BreathDamage);
+                LastAttackBreath = Time.time;
+            }
+        }
+    }
     void Rotate()
     {
         Vector3 scale = transform.localScale;
