b7795770008d5a9ec07082772fa48b6a29b2672b
JORDAN DOUGLAS <1600344@uad.ac.uk>
2019-02-13 12:53:09 +0000

    Set up door and light prefabs as well as adding static prefabs

--- Assets/Art/3DEnvironments/EnvironmentScripts/MineDoor.cs
+++ Assets/Art/3DEnvironments/EnvironmentScripts/MineDoor.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MineDoor : MonoBehaviour {
+public class MineDoor : MonoBehaviour
+{
+    public Collider2D blockingCollider;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Animator animator; // Cached variable
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("isOpen", true);
+            blockingCollider.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("isOpen", false);
+            blockingCollider.enabled = true;
+        }
+    }
 }
