f73add30418fbd8d50a5d9b5fff6e5d585736076
lewisbowie <lewisbowie@outlook.com>
2019-02-19 15:27:32 +0000

    added chest open script

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+
+    //public GameObject Collectible;
+    public float Range;
+    public Transform Player;
+    public GameObject[] objects;
+    public string tagName;
+    public Transform SpawnPoint;
+ 
+ 
+    void OpenChest()
+    {
+        GameObject Collectible = Instantiate(objects[Random.Range(0, objects.Length)], SpawnPoint.position, SpawnPoint.rotation) as GameObject;
+    }
+
+   void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == tagName)
+        {
+            OpenChest();
+            print("chest opened");
+        }
+    }
+}
+ 
