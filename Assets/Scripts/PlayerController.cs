4556ded4c44dce1bf35d53532f567478e49049f9
UAD\1502642 <lewisbowie@outlook.com>
2019-02-22 13:21:37 +0000

    added evolution script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float FlySpeed = 12.0f;
     public float JumpForce = 10.0f;
-    public float flyingTime = 2f;
+    public float flyingTime = 1f;
 
     //other variables
     //private Vector2 moveDirection = Vector2.zero;
@@ -36,6 +36,11 @@ public class PlayerController : MonoBehaviour
     public int JumpActivates;
     public int FlyActivates;
     public int BreathAttackActivates;
+    private bool Stage1;
+    private bool Stage2activate;
+    private bool Stage3activate;
+    private int Stage2 = 3;
+    private int Stage3 = 10;
 
     [Header("Player Attributes")]
     public float Health;
@@ -78,6 +83,10 @@ public class PlayerController : MonoBehaviour
         jump = Vector2.up * JumpForce;
         canJump = false;
         canFly = false;
+
+        Stage1 = true;
+        Stage2activate = false;
+        Stage3activate = false;
 
         CollectiblesCollected();
     }
@@ -144,37 +153,84 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        //sets fly on hold m
-        if (pickedUp >= FlyActivates)
+        //evolution
+        //if picked up 5 collectibles move to next sprite
+    
+        if (pickedUp >= Stage2)
         {
-            if (Input.GetKeyDown(KeyCode.M) && !canFly)
+            Stage1 = false;
+            Stage2activate = true;
+
+            if (Stage2activate)
             {
-                Fly();
-            }
+                flyingTime = 2f;
+                speed = 6f;
 
-            //returns if M is released flying stops
-            if (Input.GetKeyUp(KeyCode.M))
-            {
-                canFly = false;
-                controller.gravityScale = 5.0f;
-            }
+                if (Input.GetKeyDown(KeyCode.M) && !canFly)
+                {
+                    Fly();
+                }
 
-            if (canFly)
-            {
-                //  flyingposition.x = moveDirection.x *speed * Time.deltaTime;
-                flyingposition = transform.localPosition;
-                flyingposition.y += 2;
-
-                transform.position = Vector3.Lerp(transform.position, flyingposition, Time.deltaTime * 2);
-                flyingTimer += Time.deltaTime;
-
-                if (flyingTimer >= flyingTime)
+                //returns if M is released flying stops
+                if (Input.GetKeyUp(KeyCode.M))
                 {
                     canFly = false;
                     controller.gravityScale = 5.0f;
                 }
+
+                if (canFly)
+                {
+                    //  flyingposition.x = moveDirection.x *speed * Time.deltaTime;
+                    flyingposition = transform.localPosition;
+                    flyingposition.y += 2;
+
+                    transform.position = Vector3.Lerp(transform.position, flyingposition, Time.deltaTime * 2);
+                    flyingTimer += Time.deltaTime;
+
+                    if (flyingTimer >= flyingTime)
+                    {
+                        canFly = false;
+                        controller.gravityScale = 5.0f;
+                    }
+                }
+
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    MeleeAttack();
+                }
             }
+            //first stage can fly for 3 seconds
+            //movement is slower
         }
+
+        //if picked up 10 collectibles 
+        
+        if (pickedUp >= Stage3)
+        {
+            Stage1 = false;
+            Stage2activate = false;
+            Stage3activate = true;
+
+            if(Stage3activate)
+            {
+                flyingTime = 5f;
+                speed = 10f;
+
+                if (Input.GetKeyDown(KeyCode.X))
+                {
+                    BreathAttack();
+                }
+            }
+        
+            //movement is fast
+            //can fly for 10 seconds
+        }
+
+
+
+
+       
+      
 
         if (Input.GetKeyDown(KeyCode.O))
         {
@@ -183,17 +239,10 @@ public class PlayerController : MonoBehaviour
 
 
 
-        //Melee Attack
+    
 
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            MeleeAttack();
-
-        }
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            BreathAttack();
-        }
+      
+     
 
     }
 
