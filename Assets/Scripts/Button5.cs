63efeffe0810e7ceb036b945a13a3e0faaa07d12
SamGrant98 <samgrantarab@gmail.com>
2019-03-05 01:08:00 +0000

    Added some more animations and 3D models

---
+++ Assets/Scripts/Button5.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button5 : MonoBehaviour {
+
+    [Header("Animations")]
+    public Animator anim;
+    public Animator anim2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.Play("LiftMovement4");
+            anim2.Play("LeverMovement");
+
+
+        }
+    }
+}
